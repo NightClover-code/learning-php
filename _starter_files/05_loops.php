f3557a48f1ccc8ee33fdd5f6925a5b63cbbedef5
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-12-03 16:24:31 +0100

    feat: adding starter file examples

--- _starter_files/05_loops.php
+++ _starter_files/05_loops.php
@@ -11,6 +11,9 @@
   }
 */
 
+for ($x = 0; $x <= 10; $x++) {
+  echo "Number: $x <br>";
+}
 
 /* ------------ While Loop ------------ */
 
@@ -21,6 +24,12 @@
   }
 */
 
+$x = 1;
+
+while ($x <= 5) {
+  echo "Number: $x <br>";
+  $x++;
+}
 
 /* ---------- Do While Loop --------- */
 
@@ -33,7 +42,10 @@
 do...while loop will always execute the block of code once, even if the condition is false.
 */
 
-
+do {
+  echo "Number: $x <br>";
+  $x++;
+} while ($x <= 5);
 
 /* ---------- Foreach Loop ---------- */
 
@@ -43,3 +55,32 @@ do...while loop will always execute the block of code once, even if the conditio
   // code to be executed
   }
 */
+
+// Loop through an array
+
+$numbers = [1, 2, 3, 4, 5];
+
+foreach ($numbers as $x) {
+  echo "Number: $x <br>";
+}
+
+// Use the indexes within the loop
+
+$posts = ['Post One', 'Post Two', 'Post Three'];
+
+foreach ($posts as $index => $post) {
+  echo "${index} - ${post} <br>";
+}
+
+// Use the keys within the loop for an associative array
+
+$person = [
+  'first_name' => 'Brad',
+  'last_name' => 'Traversy',
+  'email' => 'brad@gmail.com',
+];
+
+// Get Keys
+foreach ($person as $key => $val) {
+  echo "${key} - ${val} <br>";
+}
--- _starter_files/06_functions.php
+++ _starter_files/06_functions.php
@@ -2,13 +2,56 @@
 /* ------------ Functions ----------- */
 
 /*
-  Functions are reusable blocks of code that we can call to perform a specific task.
-  We can pass values into functions to change their behavior. Functions have their own local scope as opposed to global scope
-
-
-/*
 ** Function Syntax
   function functionName($arg1, $arg2, ...) {
     // code to be executed
   }
+
+  - Functions have their own local scope as opposed to global scope
 */
+
+function registerUser()
+{
+  echo 'User has been registered!';
+}
+
+// Running a function
+registerUser();
+
+// Adding params
+function registerUser2($username)
+{
+  echo "User ${username} has been registered!";
+}
+
+// Pass in an argument
+registerUser2('Brad');
+
+// Returning values
+function add($num1, $num2)
+{
+  return $num1 + $num2;
+}
+
+$sum = add(5, 5);
+echo $sum;
+
+// Adding default values
+function subtract($num1 = 10, $num2 = 5)
+{
+  return $num1 - $num2;
+}
+
+echo subtract();
+
+// Assigning anonymous functions to variables. Often used for closures and callback functions
+$add = function ($num1, $num2) {
+  return $num1 + $num2;
+};
+
+echo $add(5, 5);
+
+// Arrow functions
+$multiply = fn($num1, $num2) => $num1 * $num2;
+
+echo $multiply(5, 5);
